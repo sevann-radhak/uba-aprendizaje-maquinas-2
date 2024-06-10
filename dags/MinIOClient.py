7e574d016d0ff0b637f91b35a3aa3b00ac578ac5
Sevann Radhak <sevann.radhak@gmail.com>
2024-06-10 20:33:00 -0300

    Etl process v2.

--- dags/MinIOClient.py
+++ dags/MinIOClient.py
@@ -3,6 +3,7 @@ from botocore.client import Config
 from botocore.exceptions import BotoCoreError, ClientError
 from boto3.s3.transfer import TransferConfig
 import os
+import io
 
 class MinioClient:
     def __init__(self, endpoint_url, access_key, secret_key):
@@ -26,7 +27,6 @@ class MinioClient:
             return False
         return True
 
-
     def save_file(self, file_name, bucket_name, object_name=None):
         if not self.bucket_exists(bucket_name):
             self.create_bucket(bucket_name)
@@ -39,10 +39,29 @@ class MinioClient:
 
         self.s3.Bucket(bucket_name).upload_file(Filename=file_name, Key=object_name, Config=config)
 
+    # def save_file_content(self, file_content, bucket_name, object_name):
+    #     if not self.bucket_exists(bucket_name):
+    #         self.create_bucket(bucket_name)
+    #     file_obj = io.BytesIO(file_content)
+    #     self.s3.Bucket(bucket_name).put_object(Key=object_name, Body=file_obj)
 
+    
+    def save_file_content(self, file_content, bucket_name, object_name):
+        if not self.bucket_exists(bucket_name):
+            self.create_bucket(bucket_name)
+        file_obj = io.BytesIO(file_content)
+        config = TransferConfig(multipart_threshold=1024**2, max_concurrency=10,
+                                multipart_chunksize=1024**2, use_threads=True)
+        self.s3.Bucket(bucket_name).upload_fileobj(Fileobj=file_obj, Key=object_name, Config=config)
+    
     def get_file(self, bucket_name, object_name):
         print(f"Downloading {object_name} from {bucket_name} bucket...")
         filename = f'downloaded_{object_name}'        
         os.makedirs(os.path.dirname(filename), exist_ok=True)
         self.s3.Bucket(bucket_name).download_file(Key=object_name, Filename=filename)
         print(f"File downloaded to {filename}")
+
+    def get_file_content(self, bucket_name, object_name):
+        obj = self.s3.Object(bucket_name, object_name)
+        file_content = obj.get()['Body'].read()
+        return file_content
--- dags/etl_process_dag.py
+++ dags/etl_process_dag.py
@@ -2,85 +2,53 @@ from airflow import DAG
 from airflow.operators.python_operator import PythonOperator
 from datetime import datetime
 import pandas as pd
-import shutil
-from io import StringIO
-import os
+import io
 from MinIOClient import MinioClient
 from FileClient import FileClient
 
-def download_file(url, src_path):
-    try:
-        downloader = FileClient()
-        downloader.download_file(url, sub_dir=os.path.dirname(src_path), filename=os.path.basename(src_path))
-        print("Data downloaded.")
+def download_file(url, dst_path):
+    downloader = FileClient()
+    file_content = downloader.download_file(url)
+    print("Data downloaded.")
 
-    except Exception as e:
-        print(f"An error occurred while downloading and copying the file: {e}")
-        
-def save_raw_data():
     minio_client = MinioClient('http://s3:9000', 'minio', 'minio123')
-    minio_client.save_file('data/raw/weatherAUS.csv', 'data', 'raw/weatherAUS.csv')
+    minio_client.save_file_content(file_content, 'data', dst_path)
     print("Data (raw) saved to MinIO.")
-  
-def clean_data(src_path, dst_path): 
-    df = pd.read_csv(src_path)
-    original_count = len(df) 
+
+def clean_data(src_path, dst_path):
+    minio_client = MinioClient('http://s3:9000', 'minio', 'minio123')
+    raw_data = minio_client.get_file_content('data', src_path)
+    df = pd.read_csv(io.BytesIO(raw_data))  
+    original_count = len(df)
     columns_to_check = df.columns.difference(['Date', 'Location'])
     df = df[df[columns_to_check].notnull().any(axis=1)]
     records_removed = original_count - len(df)
-    df.to_csv(dst_path, index=False)
+    cleaned_data = df.to_csv(index=False).encode()
     print(f"Data cleaned. {records_removed} records were removed.")
 
-def save_cleaned_data():
-    minio_client = MinioClient('http://s3:9000', 'minio', 'minio123')
-    minio_client.save_file('data/weatherAUS.csv', 'data')
+    minio_client.save_file_content(cleaned_data, 'data', dst_path)
     print("Data (cleaned) saved to MinIO.")
-    
 
-# def preview_data():
-#     file_name = 'data/weatherAUS.csv'
-#     minio_client = MinioClient('http://s3:9000', 'minio', 'minio123')
-#     minio_client.get_file('data', file_name)    
-#     print('File downloaded successfully')
-    
-#     path = f'downloaded_{file_name}'
-        
-#     if os.path.isfile(path) and os.path.getsize(path) > 0:  
-#         data = pd.read_csv(path)
-#         if not data.empty: 
-#             print(data.head(5))
-#         else:
-#             print("The CSV file is empty.")
-#     else:
-#         print(f"The file {path} does not exist or is empty.")
-    
-    
 dag = DAG('etl_dag', description='ETL DAG for WeatherAUS dataset',
           schedule_interval='0 12 * * *',
           start_date=datetime(2022, 1, 1), catchup=False)
 
-
 download_file_task = PythonOperator(
-    task_id='download_file', 
+    task_id='download_file',
     python_callable=download_file,
     op_kwargs={
         'url': "https://raw.githubusercontent.com/sevann-radhak/python-weatherAUS/aprendizaje_maquinas/weatherAUS.csv",
-        'src_path': 'data/raw/weatherAUS.csv',
-        'dst_path': 'data/weatherAUS.csv'
-    }, 
+        'dst_path': 'data/raw/weatherAUS.csv'
+    },
     dag=dag)
-save_raw_data_task = PythonOperator(task_id='save_raw_data', python_callable=save_raw_data, dag=dag)
+
 clean_data_task = PythonOperator(
-    task_id='clean_data', 
-    python_callable=clean_data, 
+    task_id='clean_data',
+    python_callable=clean_data,
     op_kwargs={
         'src_path': 'data/raw/weatherAUS.csv',
         'dst_path': 'data/weatherAUS.csv'
     },
     dag=dag)
-save_cleaned_data_task = PythonOperator(
-    task_id='save_cleaned_data', 
-    python_callable=save_cleaned_data,  
-    dag=dag)
 
-download_file_task >> save_raw_data_task >> clean_data_task >> save_cleaned_data_task #>> preview_data_task
+download_file_task >> clean_data_task
